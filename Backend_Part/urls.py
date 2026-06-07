85447c9e17d16d6363f93f15e3a8a1f22ce92c76
Lulieeliyas <lulieeliyas@gmail.com>
2026-06-07 13:16:01 +0300

    Add homepage route

--- Backend_Part/urls.py
+++ Backend_Part/urls.py
@@ -2,11 +2,26 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("""
+    <h1>University Clearance System API</h1>
+    <p>Backend is running successfully.</p>
+    <ul>
+        <li><a href="/admin/">Admin Panel</a></li>
+        <li><a href="/api/">API Endpoints</a></li>
+    </ul>
+    """)
+
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
